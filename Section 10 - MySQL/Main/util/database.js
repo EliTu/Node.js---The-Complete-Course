3e119284b114d146579ad0dec6900d6514ca4a18
EliTu <eliad91@gmail.com>
2020-06-27 17:19:53 +0300

    section 10 - lecture 142

--- Section 10 - MySQL/Main/util/database.js
+++ Section 10 - MySQL/Main/util/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    userName: 'admin',
+    user: 'admin',
     database: 'node-complete',
     password: 'Eliad@1991'
 });
