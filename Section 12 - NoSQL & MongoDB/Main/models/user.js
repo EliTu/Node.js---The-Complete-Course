f9e7f155be14880e177647baaeab44dbcd3e6d61
EliTu <eliad91@gmail.com>
2020-07-13 23:09:25 +0300

    Section 12 - lecture 197

--- Section 12 - NoSQL & MongoDB/Main/models/user.js
+++ Section 12 - NoSQL & MongoDB/Main/models/user.js
@@ -24,22 +24,34 @@ class User {
 	}
 
 	addToCart(product) {
-		const cartProduct = this.cart.items.findIndex(
-			(el) => el._id === product._id
+		const cartProductIndex = this.cart.items.findIndex(
+			(el) => el.productId.toString() === product._id.toString()
 		);
+		let newQuantity = 1;
+		const updatedCartItems = [...this.cart.items];
+		let updatedCart;
+		const db = getDb();
 
-		if (!cartProduct !== -1) {
-			const updatedCart = {
-				items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
-			};
-			const db = getDb();
-
-			const res = db
-				.collection('users')
-				.updateOne({ _id: this._id }, { $set: { cart: updatedCart } });
-
-			return res;
+		// If cartProduct yields -1 index, set a new cart with product
+		if (cartProductIndex === -1) {
+			updatedCartItems.push({
+				productId: new mongodb.ObjectId(product._id),
+				quantity: newQuantity,
+			});
+		} else {
+			// else update the cart's item quantity
+			newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+			updatedCartItems[cartProductIndex].quantity = newQuantity;
 		}
+		updatedCart = {
+			items: updatedCartItems,
+		};
+
+		const res = db
+			.collection('users')
+			.updateOne({ _id: this._id }, { $set: { cart: updatedCart } });
+
+		return res;
 	}
 }
 
