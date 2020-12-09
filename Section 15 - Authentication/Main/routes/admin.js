0fbb95cdc92a55e55c6b07af3417bd1454f1f96a
EliTu <eliad91@gmail.com>
2020-12-09 05:57:04 +0200

    Section 15 lecture 258

--- Section 15 - Authentication/Main/routes/admin.js
+++ Section 15 - Authentication/Main/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const isAuthenticated = require('../middleware/isAuthenticated');
 
 // Controller import
 const {
@@ -10,14 +11,16 @@ const {
 	postDeleteProduct,
 } = require('../controllers/adminController');
 
+/* the middleware parse the arguments from left to right, so we can add as many middlewares, like isAuthenticated, before the final resolving function as we like*/
+
 // /admin/add-product => GET
-router.get('/add-product', getAddProduct);
-router.get('/admin-products', getAdminProduct);
-router.get('/edit-product/:productId', getEditProduct);
+router.get('/add-product', isAuthenticated, getAddProduct);
+router.get('/admin-products', isAuthenticated, getAdminProduct);
+router.get('/edit-product/:productId', isAuthenticated, getEditProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', postProduct);
-router.post('/edit-product', postProduct);
-router.post('/delete-product', postDeleteProduct);
+router.post('/add-product', isAuthenticated, postProduct);
+router.post('/edit-product', isAuthenticated, postProduct);
+router.post('/delete-product', isAuthenticated, postDeleteProduct);
 
 module.exports = router;
--- Section 15 - Authentication/Main/routes/shop.js
+++ Section 15 - Authentication/Main/routes/shop.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const isAuthenticated = require('../middleware/isAuthenticated');
 
 const {
 	getAllProducts,
@@ -14,15 +14,17 @@ const {
 	postOrder,
 } = require('../controllers/shopController');
 
+/* the middleware parse the arguments from left to right, so we can add as many middlewares, like isAuthenticated, before the final resolving function as we like*/
+
 router.get('/', getShopPage);
 router.get('/products', getAllProducts);
 router.get('/products/:productId', getProductDetailsPage);
-router.get('/cart', getCartPage);
-router.get('/orders', getOrdersPage);
-router.get('/checkout', getCheckoutPage);
+router.get('/cart', isAuthenticated, getCartPage);
+router.get('/orders', isAuthenticated, getOrdersPage);
+router.get('/checkout', isAuthenticated, getCheckoutPage);
 
-router.post('/cart', postCart);
-router.post('/cart-delete-product', postCartDeleteProduct);
-router.post('/create-order', postOrder);
+router.post('/cart', isAuthenticated, postCart);
+router.post('/cart-delete-product', isAuthenticated, postCartDeleteProduct);
+router.post('/create-order', isAuthenticated, postOrder);
 
 module.exports = router;
