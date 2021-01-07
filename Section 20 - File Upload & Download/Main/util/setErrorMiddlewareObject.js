265f848049516e43c559b68f14aa8bd0ecdbed6e
EliTu <eliad91@gmail.com>
2021-01-07 07:02:21 +0200

    section 20 lecture 328

--- Section 20 - File Upload & Download/Main/util/setErrorMiddlewareObject.js
+++ Section 20 - File Upload & Download/Main/util/setErrorMiddlewareObject.js
@@ -1,5 +1,6 @@
 // set a new error object to handle server errors that were caught by the catch block and pass the error object to next to let express handle errors
 const setErrorMiddlewareObject = (error = 'Server error has ocurred', next) => {
+	// TODO: WORK ON THE ERROR OBJECT AND 500 VIEW PAGE
 	const errorObject = new Error(error);
 	errorObject.httpStatusCode = 500;
 
