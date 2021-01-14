0284ec96129fd6adeef4f05a67ad040a420f9fd3
EliTu <eliad91@gmail.com>
2021-01-14 06:10:22 +0200

    section 22 lecture 348

--- Section 22 - Understanding Async Requests/Main/public/js/domUtils.js
+++ Section 22 - Understanding Async Requests/Main/public/js/domUtils.js
@@ -1,7 +1,11 @@
+// Queries
+const message = document.querySelector('.message');
+const filePicker = document.querySelector('.file-picker-input');
+const deleteButton = document.querySelector('#product-delete-button');
+
 /**
  * a simple util to find and hide the popout success or error message in the UI after 8 seconds.
  */
-const message = document.querySelector('.message');
 if (message) {
 	setTimeout(() => {
 		message.style.display = 'none';
@@ -11,7 +15,6 @@ if (message) {
 /**
  *  a util to set the file picker input label text value when picking a file with the input.
  * */
-const filePicker = document.querySelector('.file-picker-input');
 if (filePicker) {
 	filePicker.onchange = () => {
 		if (filePicker.files.length > 0) {
@@ -20,3 +23,22 @@ if (filePicker) {
 		}
 	};
 }
+
+/**
+ * a util function to delete a product from the admin products list.
+ * @param {*} btn The button input that was clicked, accessed via the native onclick function.
+ */
+const deleteProduct = (btn) => {
+	// access the clicked button element parent first
+	const buttonParent = btn.parentNode;
+
+	// get the data hidden inputs
+	const hiddenInputs = buttonParent.querySelectorAll('[type="hidden"]');
+	const inputValues = {};
+
+	// create a mapped object of the values by input name
+	for (const { name, value } of hiddenInputs) {
+		inputValues[name] = value;
+	}
+	console.log(inputValues);
+};
