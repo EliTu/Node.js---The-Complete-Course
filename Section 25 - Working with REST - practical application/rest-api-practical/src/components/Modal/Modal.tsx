98c735ddebe603e65ee5c0c19597637f28cf5063
EliTu <eliad91@gmail.com>
2021-02-20 13:25:09 +0200

    refactoring Modal

---
+++ Section 25 - Working with REST - practical application/rest-api-practical/src/components/Modal/Modal.tsx
@@ -0,0 +1,50 @@
+import ReactDOM from 'react-dom';
+
+import Button from '../Button/Button';
+import './Modal.css';
+
+interface ModalProps {
+	title: string;
+	isLoading: boolean;
+	acceptEnabled: boolean;
+	onAcceptModal: () => void;
+	onCancelModal: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({
+	acceptEnabled,
+	isLoading,
+	onAcceptModal,
+	onCancelModal,
+	title,
+	children,
+}) => {
+	const modalRoot = document.getElementById('modal-root');
+	return (
+		modalRoot &&
+		ReactDOM.createPortal(
+			<div className='modal'>
+				<header className='modal__header'>
+					<h1>{title}</h1>
+				</header>
+				<div className='modal__content'>{children}</div>
+				<div className='modal__actions'>
+					<Button design='danger' mode='flat' onClick={onCancelModal}>
+						Cancel
+					</Button>
+					<Button
+						mode='raised'
+						onClick={onAcceptModal}
+						disabled={!acceptEnabled}
+						loading={isLoading}
+					>
+						Accept
+					</Button>
+				</div>
+			</div>,
+			modalRoot
+		)
+	);
+};
+
+export default Modal;
